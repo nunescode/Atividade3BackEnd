9c04660cedd9786a34b70b386b6578332e65eabb
nunescode <103227499+nunescode@users.noreply.github.com>
2022-09-06 20:25:34 -0300

    Update TurmaAluno.ts

--- Documents/cb/Iesb/app/Models/TurmaAluno.ts
+++ Documents/cb/Iesb/app/Models/TurmaAluno.ts
@@ -4,6 +4,12 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 export default class TurmaAluno extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+  
+  @column()
+  public turmas_id: number
+  
+  @column()
+  public alunos_id: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
